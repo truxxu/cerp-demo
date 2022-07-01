8fbab0e80398175f024f4dbc422b055893d59641
truxxu <danielfetrujillo@gmail.com>
2022-07-01 11:41:41 -0500

    created input component

---
+++ src/molecules/Input.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import {StyleSheet, Text, View, TextInput} from 'react-native';
+import {Controller} from 'react-hook-form';
+
+import {fonts, colors, margin, padding} from '../styles/base.js';
+
+const Input = ({name, label, placeholder, keyboard, control}) => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.label}>{label}</Text>
+      <Controller
+        control={control}
+        rules={{
+          required: true,
+        }}
+        render={({field: {onChange, onBlur, value}}) => (
+          <TextInput
+            style={styles.input}
+            onBlur={onBlur}
+            onChangeText={onChange}
+            value={value}
+            keyboardType={keyboard}
+            placeholder={placeholder}
+            placeholderTextColor={colors.disabled2}
+          />
+        )}
+        name={name}
+      />
+    </View>
+  );
+};
+
+export {Input};
+
+const styles = StyleSheet.create({
+  container: {
+    marginVertical: margin.md,
+  },
+  label: {
+    color: colors.text,
+    fontSize: fonts.sm,
+    fontFamily: fonts.primary,
+    marginBottom: margin.xs,
+  },
+  input: {
+    fontSize: fonts.sm,
+    fontFamily: fonts.primary,
+    borderWidth: 1,
+    borderRadius: 10,
+    borderColor: colors.disabled2,
+    paddingHorizontal: padding.md,
+  },
+});
