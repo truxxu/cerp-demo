3f033fe8ea4814238caa77120105817a0c628edc
2624789 <2624789@users.noreply.github.com>
2022-07-01 14:21:50 -0500

    Added inputs to login screen

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,12 +1,27 @@
 import React, {useState} from 'react';
 import {Image, Pressable, Text, StyleSheet, View} from 'react-native';
+import {useForm} from 'react-hook-form';
 
 import {ScreenTemplate, Button, Modal} from '../atoms';
-import {fonts, colors} from '../styles/base.js';
+import {Input} from '../molecules';
+import {fonts, colors, margin} from '../styles/base.js';
 
 import Logo from './../images/logo.png';
 
 const Login = ({navigation}) => {
+  const {
+    control,
+    handleSubmit,
+    reset,
+    formState: {isDirty, isValid},
+  } = useForm({
+    mode: 'onChange',
+  });
+
+  const onSubmit = () => {
+    setIsModalVisible(true);
+  };
+
   const [isModalVisible, setIsModalVisible] = useState(false);
 
   const verificationModal = () => {
@@ -25,7 +40,30 @@ const Login = ({navigation}) => {
       {verificationModal()}
       <Image style={styles.image} source={Logo} />
       <Text style={[styles.text, styles.title]}>¡Bienvenido!</Text>
-      <Button label="Iniciar Sesión" action={() => setIsModalVisible(true)} />
+
+      <View style={styles.formContainer}>
+        <Input
+          name="id"
+          label="Cédula"
+          placeholder="Ingresa tu número de cédula"
+          keyboard="numeric"
+          control={control}
+        />
+        <Input
+          name="password"
+          label="Contraseña"
+          placeholder="Ingresa una contraseña"
+          keyboard="email-address"
+          control={control}
+        />
+      </View>
+
+      <Button
+        label="Iniciar Sesión"
+        action={handleSubmit(onSubmit)}
+        disabled={!isDirty || !isValid}
+      />
+
       <Pressable onPress={() => navigation.navigate('register')}>
         <Text style={styles.link}>Registrarse</Text>
       </Pressable>
@@ -49,9 +87,14 @@ const styles = StyleSheet.create({
   link: {
     color: colors.primary,
     fontSize: fonts.sm,
+    marginTop: margin.md,
   },
   image: {
     height: 100,
     resizeMode: 'contain',
   },
+  formContainer: {
+    width: '100%',
+    marginVertical: margin.xl,
+  }
 });
