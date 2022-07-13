d4fe945c5e6acaa7e34acfff0d2424006a45ecef
truxxu <danielfetrujillo@gmail.com>
2022-07-13 07:43:32 -0500

    added qr scanning lib

--- src/screens/Pay.js
+++ src/screens/Pay.js
@@ -1,9 +1,55 @@
 import React from 'react';
+import {StyleSheet, Text, TouchableOpacity} from 'react-native';
+// import QRCodeScanner from 'react-native-qrcode-scanner';
+// import {RNCamera} from 'react-native-camera';
 
 import {ScreenTemplate} from '../atoms';
 
 const Pay = () => {
-  return <ScreenTemplate></ScreenTemplate>;
+  const onSuccess = e => {
+    console.log('success!');
+  };
+
+  return (
+    <ScreenTemplate>
+      {/* <QRCodeScanner
+        onRead={onSuccess}
+        // flashMode={RNCamera.Constants.FlashMode.torch}
+        topContent={
+          <Text style={styles.centerText}>
+            Go to{' '}
+            <Text style={styles.textBold}>wikipedia.org/wiki/QR_code</Text> on
+            your computer and scan the QR code.
+          </Text>
+        }
+        bottomContent={
+          <TouchableOpacity style={styles.buttonTouchable}>
+            <Text style={styles.buttonText}>OK. Got it!</Text>
+          </TouchableOpacity>
+        }
+      /> */}
+    </ScreenTemplate>
+  );
 };
 
 export {Pay};
+
+const styles = StyleSheet.create({
+  centerText: {
+    flex: 1,
+    fontSize: 18,
+    padding: 32,
+    color: '#777',
+  },
+  textBold: {
+    fontWeight: '500',
+    color: '#000',
+  },
+  buttonText: {
+    fontSize: 21,
+    color: 'rgb(0,122,255)',
+  },
+  buttonTouchable: {
+    padding: 16,
+  },
+});
