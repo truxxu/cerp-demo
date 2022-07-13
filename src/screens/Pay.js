7bbf86b32e7f585ffebfa7f0962584fca62a0f1a
truxxu <danielfetrujillo@gmail.com>
2022-07-12 20:17:01 -0500

    test

--- src/screens/Pay.js
+++ src/screens/Pay.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import {View, Text} from 'react-native';
+import QRCodeScanner from 'react-native-qrcode-scanner';
 
 const Pay = () => {
+  const onSuccess = e => {
+    console.log('success!');
+  };
+
   return (
     <View>
-      <Text>Pay</Text>
+      <QRCodeScanner onRead={onSuccess} showMarker={true} />
     </View>
   );
 };
